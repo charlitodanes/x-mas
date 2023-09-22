8b61c61da1962864a6704b3272ec5e36e8a03fed
Charlito Danes <91553202+charlitodanes@users.noreply.github.com>
2023-09-23 01:40:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let daysEl = document.getElementById("days");
+let hrsEl = document.getElementById("hrs");
+let minsEl = document.getElementById("mins");
+let secsEl = document.getElementById("secs");
+
+function pasko() {
+  const xmas = new Date("2023-12-25 00:00:00").getTime();
+  const now = new Date().getTime();
+  const days_left = xmas - now;
+  const days = Math.floor(days_left / (1000 * 60 * 60 * 24));
+  const hrs = Math.floor((days_left % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const mins = Math.floor((days_left % (1000 * 60 * 60)) / (1000 * 60));
+  const secs = Math.floor((days_left % (1000 * 60)) / 1000);
+
+  daysEl.innerHTML = days;
+  hrsEl.innerHTML = hrs;
+  minsEl.innerHTML = mins;
+  secsEl.innerHTML = secs;
+  if (days_left <= 0) {
+    alert("Pasko na!");
+  } 
+}
+pasko();
+
+setInterval(pasko, 1000);
+
+const backgroundVideo = document.getElementById("myVideo");
+
+    // Activates the event listener when the background video is already playing
+    backgroundVideo.addEventListener("play", function () {
+        alert("Click anywhere on the page for a surprise :)");
+});
+
+const body = document.getElementById("body");
+const audio = document.getElementById("myAudio");
+let isAudioPlaying = false;
+
+body.addEventListener("click", function () {
+  if (isAudioPlaying) {
+    audio.pause();
+    isAudioPlaying = false;
+  } else {
+    audio.muted = false;
+    audio.play();
+    isAudioPlaying = true;
+  }
+});
+
+
