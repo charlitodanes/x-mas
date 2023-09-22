d8399a4d306ea8cf06700e7636f159c384305374
Deus Ex Machina <charlitodanes@gmail.com>
2023-09-23 02:05:40 +0800

    adjust word-spacing in the title

--- script.js
+++ script.js
@@ -24,12 +24,12 @@ pasko();
 
 setInterval(pasko, 1000);
 
-// const backgroundVideo = document.getElementById("myVideo");
+const backgroundVideo = document.getElementById("myVideo");
 
-//     // Activates the event listener when the background video is already playing
-//     backgroundVideo.addEventListener("play", function () {
-//         alert("Click anywhere on the page for a surprise :)");
-// });
+    // Activates the event listener when the background video is already playing
+    backgroundVideo.addEventListener("play", function () {
+        alert("Click anywhere on the page for a surprise :)");
+});
 
 const body = document.getElementById("body");
 const audio = document.getElementById("myAudio");
